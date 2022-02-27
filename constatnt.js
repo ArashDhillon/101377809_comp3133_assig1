1dbdffd4e3dd29fd9c435feb638b2c0b85049531
Arashdeep <arashdeep.dhillon@georgebrown.ca>
2022-02-26 20:15:20 -0600

    commit 1

---
+++ constatnt.js
@@ -0,0 +1,7 @@
+var uri = "mongodb://students:students@cluster0-shard-00-00.fivtn.mongodb.net:27017,cluster0-shard-00-01.fivtn.mongodb.net:27017,cluster0-shard-00-02.fivtn.mongodb.net:27017/101377809_comp3133_assig1?ssl=true&replicaSet=atlas-lzi1dj-shard-0&authSource=admin&retryWrites=true&w=majority";
+
+module.exports = {
+  debug_name: 'graphql',
+  secrectKey: 'studentId_comp3133_assig1',
+  uri_mongodb: uri
+  };
